1b30fcedb70d905505bdb33a0cdb4f632452a34e
spiralcode5 <96902560+spiralcode5@users.noreply.github.com>
2022-07-26 12:42:36 +0530

    stud_API

---
+++ restapicrud/studentapi/models.py
@@ -0,0 +1,11 @@
+import email
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)
+    address = models.CharField(max_length=30)
+
+
